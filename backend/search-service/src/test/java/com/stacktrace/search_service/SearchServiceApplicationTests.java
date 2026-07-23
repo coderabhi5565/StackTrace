40d157be0c785543483c3423ba854043944e4926
coderabhi5565 <gabhinav084@gmail.com>
2026-07-23 13:32:12 +0530

    Added Search Service

---
+++ backend/search-service/src/test/java/com/stacktrace/search_service/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacktrace.search_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
