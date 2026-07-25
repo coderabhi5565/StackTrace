55793cf4a39d86cebd7fc5296bd1740e0677668d
coderabhi5565 <gabhinav084@gmail.com>
2026-07-25 15:48:41 +0530

    Added And Started Notification Service

---
+++ backend/NotificationService/src/main/java/com/notification_service/StackTrace/StackTraceApplication.java
@@ -0,0 +1,13 @@
+package com.notification_service.StackTrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackTraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackTraceApplication.class, args);
+	}
+
+}
